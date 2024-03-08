5150dbcd2fdd1c6b99233d3afbcfcf168549851c
AgueroRDev <robertd.aguero23@gmail.com>
2024-03-08 00:41:51 -0500

    Archivo 1 http

--- 1-http.js
+++ 1-http.js
@@ -1,16 +1,35 @@
-const {findAvaiblePort} = require('./12-Puerto-Disponible')
-const http = require('node:http')
+const http = require('node:http'); // PROTOCOLO HTTTP
+const fs = require('node:fs');
 
-console.log(process.env);
+const desiredPort = process.env.PORT ?? 1234
 
-const server = http.createServer((req,res)=>{
-    console.log('request received')
-    res.end("HOLITAS GATO PANDAAA ")
+const processReq = (req,res)=>{
+    if (req.url === '/') {
+        res.statusCode = 200 // Ok
+        res.setHeader('Content-Type','text/html')
+        res.end("Hola Gatete")
+    }else if(req.url ==='/pink'){
+        fs.readFile('./Dark.png',(err,data)=>{
+            if(err){
+                res.statusCode = 500 // Error
+                res.end("Error 500")
+            }else{
+                res.setHeader('Content-Type','image/png')
+                res.end(data)
+            }
+        }) 
+
+    }else if(req.url==='/contacto'){
+        res.statusCode = 200 //ok
+        res.end('<h1>SOY LA PAG CONTACTO </h1>')
+    }else {
+        res.statusCode = 404 //Not Found
+        res.end('<h1> 404 PRRRO </h1>')
+    }
+    
+}
+const server = http.createServer(processReq)
+
+server.listen(desiredPort,()=>{
+    console.log(`Server Escuchando en el Puerto http://localhost:${desiredPort}`);
 })
-
-findAvaiblePort(4000).then(port =>{
-    server.listen(port,()=>{
-        console.log(`EL SERVER ESTA CORRIENDO EN http://localhost:${server.address().port}`);
-    })
-})
-
